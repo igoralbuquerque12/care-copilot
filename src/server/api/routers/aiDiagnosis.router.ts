d7c6bcd6a2cd048512c5ba26ae7055631db75ff3
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-09-01 09:11:12 -0300

    feat(api): add TRPC router for AI diagnosis settings and operations

---
+++ src/server/api/routers/aiDiagnosis.router.ts
@@ -0,0 +1,46 @@
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import {
+  analysisByAnamnesisSchema,
+  removeCredentialSchema,
+  saveAnalysisSettingsSchema,
+  saveCredentialSchema,
+  testCredentialSchema,
+} from "~/schemas/ai-analysis";
+import * as analysisService from "~/server/services/aiDiagnosis";
+import * as settingsService from "~/server/services/aiDiagnosis/settings";
+
+export const aiDiagnosisRouter = createTRPCRouter({
+  getSettings: protectedProcedure.query(({ ctx }) =>
+    settingsService.getSettings(ctx.db, ctx.user.id),
+  ),
+  saveCredential: protectedProcedure
+    .input(saveCredentialSchema)
+    .mutation(({ ctx, input }) =>
+      settingsService.saveCredential(ctx.db, ctx.user.id, input.provider, input.apiKey),
+    ),
+  removeCredential: protectedProcedure
+    .input(removeCredentialSchema)
+    .mutation(({ ctx, input }) =>
+      settingsService.removeCredential(ctx.db, ctx.user.id, input.provider),
+    ),
+  testCredential: protectedProcedure
+    .input(testCredentialSchema)
+    .mutation(({ ctx, input }) =>
+      settingsService.testCredential(ctx.db, ctx.user.id, input.provider, input.model),
+    ),
+  saveSettings: protectedProcedure
+    .input(saveAnalysisSettingsSchema)
+    .mutation(({ ctx, input }) =>
+      settingsService.saveSettings(ctx.db, ctx.user.id, input),
+    ),
+  getByAnamnesis: protectedProcedure
+    .input(analysisByAnamnesisSchema)
+    .query(({ ctx, input }) =>
+      analysisService.getAnalysisByAnamnesis(ctx.db, ctx.user.id, input.anamnesisId),
+    ),
+  retry: protectedProcedure
+    .input(analysisByAnamnesisSchema)
+    .mutation(({ ctx, input }) =>
+      analysisService.retryAnalysis(ctx.db, ctx.user.id, input.anamnesisId),
+    ),
+});
